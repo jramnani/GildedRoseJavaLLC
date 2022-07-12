d638c0d71a1bf497cbb21dc0c3b8a78e9d2bbfde
jonmanahan <jmanahan@8thlight.com>
2022-07-12 16:22:10 -0400

    Extracted item arguments as variables

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,98 +9,158 @@ class GildedRoseTest {
 
     @Test
     void qualityIsNeverNegative() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        String name = "foo";
+        int sellInDays = 0;
+        int quality = 0;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertTrue(app.items[0].quality >= 0);
     }
 
     @Test
     void qualityIsNeverMoreThan50() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 50) };
+        String name = "Aged Brie";
+        int sellInDays = 0;
+        int quality = 50;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertTrue(app.items[0].quality <= 50);
     }
 
     @Test
-    void qualityDegradesTwiceAsFastWhenPassedSellBy() {
-        Item[] items = new Item[] { new Item("FLAvorTown", 0, 50) };
+    void qualityDegradesTwiceAsFastWhenPassedSellIn() {
+        String name = "FlavorTown";
+        int sellInDays = 0;
+        int quality = 50;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(48, app.items[0].quality);
     }
 
     @Test
     void updateNormalItem_QualityShouldReduceBy1() {
-        Item[] items = new Item[] { new Item("FLAvorTown", 7, 50) };
+        String name = "FlavorTown";
+        int sellInDays = 7;
+        int quality = 50;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(49, app.items[0].quality);
     }
 
     @Test
-    void updateNonLegendaryItem_SellByWillReduceBy1() {
-        Item[] items = new Item[] { new Item("FLAvorTown", 7, 50) };
+    void updateNonLegendaryItem_SellInWillReduceBy1() {
+        String name = "FlavorTown";
+        int sellInDays = 7;
+        int quality = 50;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(6, app.items[0].sellIn);
     }
 
     @Test
     void updateAgedBrie_QualityShouldIncreaseBy1() {
-        Item[] items = new Item[] { new Item("Aged Brie", 7, 7) };
+        String name = "Aged Brie";
+        int sellInDays = 7;
+        int quality = 7;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(8, app.items[0].quality);
     }
 
     @Test
-    void updateAgedBrie_QualityShouldIncreaseBy2WhenPastSellBy() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 7) };
+    void updateAgedBrie_QualityShouldIncreaseBy2WhenPastSellIn() {
+        String name = "Aged Brie";
+        int sellInDays = 0;
+        int quality = 7;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(9, app.items[0].quality);
     }
 
     @Test
     void legendaryItemDetails_ShouldNeverUpdate() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 7, 80) };
+        String name = "Sulfuras, Hand of Ragnaros";
+        int sellInDays = 7;
+        int quality = 80;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(80, app.items[0].quality);
         assertEquals(7, app.items[0].sellIn);
     }
 
     @Test
     void backstagePassesQualityIncreasesBy1_MoreThan10DaysAway() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 12, 32) };
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellInDays = 12;
+        int quality = 32;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(33, app.items[0].quality);
     }
 
     @Test
     void backstagePassesQualityIncreasesBy2_When6To10DaysAway() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 6, 32) };
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellInDays = 6;
+        int quality = 32;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(34, app.items[0].quality);
     }
 
     @Test
     void backstagePassesQualityIncreasesBy3_When1To5DaysAway() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 1, 32) };
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellInDays = 1;
+        int quality = 32;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(35, app.items[0].quality);
     }
 
     @Test
     void backstagePassesQualityDropsTo0_WhenSellInEquals0() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 32) };
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellInDays = 0;
+        int quality = 32;
+        Item[] items = new Item[] { new Item(name, sellInDays, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals(0, app.items[0].quality);
     }
 
