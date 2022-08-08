fdd9f3bd778cf56f3414c12354a8eb0c5e5949a4
Ismail Karabulut <i.karabulut93@gmail.com>
2022-08-08 14:38:40 -0500

    adds tests around displayed items from inventory

--- gilded-rose-llc-frontend/__tests__/index.tests.tsx
+++ gilded-rose-llc-frontend/__tests__/index.tests.tsx
@@ -1,12 +1,48 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Home from "../pages/index";
+import Home from '../pages/index'
 
-describe("Home", () => {
-  it("should render a heading of Gilded Rose", () => {
-    render(<Home items={[]}/>);
+describe('Home', () => {
+  it('should render a heading of Gilded Rose', () => {
+    render(<Home items={[]} />)
 
-    const heading = screen.getByRole('heading', { name: /Gilded Rose/i });
+    const heading = screen.getByRole('heading', { name: /Gilded Rose/i })
     expect(heading).toBeInTheDocument()
-  });
-});
+  })
+
+  it('should render all of the items on the page', () => {
+    const items = [
+      {
+        name: 'aged brie',
+        price: '100',
+      },
+      {
+        name: 'conjured',
+        price: '50',
+      },
+    ]
+    render(<Home items={items} />)
+    const renderedItems = screen.getAllByRole('listitem')
+
+    expect(renderedItems.length).toEqual(items.length)
+  })
+
+  it('should render the name and price of each item on the page', () => {
+    const items = [
+      {
+        name: 'aged brie',
+        price: '100',
+      },
+      {
+        name: 'conjured',
+        price: '50',
+      },
+    ]
+    render(<Home items={items} />)
+
+    items.forEach((item) => {
+      expect(screen.getByText(item.name)).toBeInTheDocument()
+      expect(screen.getByText(item.price)).toBeInTheDocument()
+    })
+  })
+})
