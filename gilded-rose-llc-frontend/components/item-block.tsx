304530a165714031c4213c8aeb05b8d28f2416ba
Ismail Karabulut <i.karabulut93@gmail.com>
2022-08-11 20:30:01 -0500

    first iteration of design + image

--- gilded-rose-llc-frontend/components/item-block.tsx
+++ gilded-rose-llc-frontend/components/item-block.tsx
@@ -5,12 +5,10 @@ interface ItemBlockProps {
 
 const ItemBlock = ({ name, price }: ItemBlockProps) => {
   return (
-    <div className="box-border px-10 py-3 bg-[#8690F4] flex flex-col items-center p-5 justify-between">
-      <p className="text-[24px] font-roboto text-center">{name}</p>
-      <p className="text-[16px] font-roboto font-semibold text-center">
-        {price}
-      </p>
-      <button className="text-[16px] font-roboto font-semibold border-solid border-2 rounded-[5px] min-w-fit">
+    <div className="rounded-[5px] box-border px-10 py-6 h-full space-y-5 bg-[#8690F4] flex flex-col items-center justify-between">
+      <p className="text-[24px] font-roboto font-[700] text-center">{name}</p>
+      <p className="text-[16px] font-roboto font-[600] text-center">${price}</p>
+      <button className="text-[16px] font-roboto font-[600] border-solid border-2 rounded-[5px] py-1 px-3 min-w-fit">
         add to cart
       </button>
     </div>
--- gilded-rose-llc-frontend/pages/index.tsx
+++ gilded-rose-llc-frontend/pages/index.tsx
@@ -3,6 +3,8 @@ import { GetServerSideProps } from 'next'
 import ItemBlock from 'components/item-block'
 import { Item } from 'core/item'
 import * as api from 'core/client'
+import Image from 'next/image'
+import logo from 'asset/images/logo.svg'
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const gateway = new api.ApiClient(fetch)
@@ -16,12 +18,19 @@ interface HomeProps {
 
 const Home: NextPage<HomeProps> = ({ items }) => {
   return (
-    <div className="bg-[#222C40] text-white w-full h-full">
-      <div className="flex-col w-4/5">
-        <div className="text-[40px] font-roboto">Gilded Rose</div>
-        <p className="">available items</p>
+    <div className="box-border px-14 pt-5 pb-10 items-center justify-between bg-[#222C40] text-white w-full h-full">
+      <div className="flex-col">
+        <div className="items-center flex pt-10 pb-14 text-[40px] font-roboto font-[700]">
+          <div className="h-20 w-24">
+            <Image src={logo} alt="gilded rose logo" />
+          </div>
+          <p className="pl-4">gilded rose</p>
+        </div>
+        <p className="pt-8 pb-12 text-[32px] font-roboto font-[600]">
+          available items
+        </p>
         <div className="">
-          <section className="grid grid-cols-3 gap-4">
+          <section className="grid grid-cols-3 gap-x-6 gap-y-8">
             {items.map((item, index) => (
               <ItemBlock key={index} name={item.name} price={item.price} />
             ))}
