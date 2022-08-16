fbf7dbda1e5654a4c8d670b4b7f02153eba16571
jonmanahan <jmanahan@8thlight.com>
2022-08-16 17:00:58 -0400

    Fix linting error

--- gilded-rose-llc-frontend/components/layout.tsx
+++ gilded-rose-llc-frontend/components/layout.tsx
@@ -1,7 +1,7 @@
 import Header from 'components/header'
 import { PropsWithChildren } from 'react'
 
-const Layout = ({ children }: PropsWithChildren<{}>) => (
+const Layout = ({ children }: PropsWithChildren) => (
     <div className="box-border px-14 pt-5 pb-10 bg-[#222C40] text-white min-h-screen flex-col">
         <Header />
         {children}
